2e8da7d16fa9a286e06a9187e41cf4379d54180b
abenaicha <aya.ben_aicha@bordeaux-inp.fr>
2024-01-23 15:04:52 +0100

    partie1

--- eventStore.js
+++ eventStore.js
@@ -3,11 +3,103 @@ const Event = require('./event');
 
 let eventList = []; 
 
-function addEvent(name, patientId, payload, creationDate) {
-    // Créer le  nouvel événement et l'ajouter à eventList
-    const newEvent = new Event(name, patientId, payload, creationDate);
-    eventList.push(newEvent);
-}
 
 
-module.exports = {addEvent,eventList};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
